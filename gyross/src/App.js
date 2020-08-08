ef7c945cc2a3655a5bfbdc823715749b18a76e79
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-08-08 13:35:11 -0400

    New routes added for customer login/register

--- gyross/src/App.js
+++ gyross/src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import LoginSignup from "./pages/login-signup/vendor/Login_Signup.vendor";
 import VendorLogin from "./pages/login/vendor-login/Vendor.login.page";
+import BuyerLogin from "./pages/login/customer-login/Customer.login.page";
 import VendorSignup from "./pages/signup/vendor-signup/Vendor.signup.page";
+import BuyerSignup from "./pages/signup/customer-signup/Customer.signup.page";
+
 import "./App.css";
 
 import Layout from "./layout/Layout";
@@ -14,7 +17,9 @@ function App() {
         <Route path="/" exact component={Container} />
         <Route path="/vendor-login" exact component={VendorLogin} />
         <Route path="/vendor-signup" exact component={VendorSignup} />
-        <Route path="/login_Signup" exact component={LoginSignup} />
+        <Route path="/buyer-login" exact component={BuyerLogin} />
+        <Route path="/buyer-Signup" exact component={BuyerSignup} />
+        <Route path="/login-Signup" exact component={LoginSignup} />
       </Switch>
     </Layout>
   );
