ab20de9a138f908d549975e894f5dad1138ec9e9
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-08-09 14:11:44 -0400

    Carousel added to container comp

--- gyross/src/container/Container.js
+++ gyross/src/container/Container.js
@@ -1,18 +1,37 @@
 import React from "react";
 import FormButton from "../utils/buttons/FormButtons";
+import CustomCarousel from "../utils/carousel/Carousel";
 import "./Container.scss";
 
 const Body = () => {
+  const imageArr = [
+    {
+      title: "Foodcart",
+      src:
+        "https://images.unsplash.com/photo-1530649325239-1c2ebf1e3f07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+      description: "Bmcc Manhattan",
+    },
+    {
+      title: "Foodcart",
+      src:
+        "https://images.unsplash.com/photo-1547620917-786ebcbc55af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+      description: "Late night best Gyross",
+    },
+    {
+      title: "Foodcart",
+      src:
+        "https://images.unsplash.com/photo-1471110338536-858caa3dbe45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+      description: "Can't be better food out there in cheap price",
+    },
+  ];
   return (
     <div className="body-container">
       <div className="top-first">
-        <img
-          className="cart-pic"
-          alt="gyro cart"
-          src="https://images.unsplash.com/photo-1530649325239-1c2ebf1e3f07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
-        />
-        <div>
-          <p className="paragraph">
+        <div className="carousel-container">
+          <CustomCarousel arr={imageArr} />
+        </div>
+        <div className="paragraph">
+          <p>
             Are you the only one who goes to the ATM to get cash just to eat a
             Halal cart? No you are not. So Order in "Gyros" and stop thinking
             about cash.
