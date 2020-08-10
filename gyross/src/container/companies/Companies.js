4c291622d7505953ce6d4a5803c180bad0a8673b
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-08-10 19:19:55 -0400

    Dummy Company content added

---
+++ gyross/src/container/companies/Companies.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Companies.style.scss";
+
+const Companies = () => {
+  return (
+    <div className="companies_container">
+      <div>
+        <h1 className="title">Try these best halal food out there.</h1>
+      </div>
+      <div className="companies_label">
+        <h3 className="companies_name">Halal Guys</h3>
+        <h3 className="companies_name">Omor Halal</h3>
+        <h3 className="companies_name">Shah Halal</h3>
+        <h3 className="companies_name">Halal Vendor</h3>
+        <h3 className="companies_name">Halal Guys</h3>
+        <h3 className="companies_name">Halal universe</h3>
+        <h3 className="companies_name">Halal guys</h3>
+        <h3 className="companies_name">Halal Restaurant</h3>
+        <h3 className="companies_name">Tunir Mar Halal Food</h3>
+        <h3 className="companies_name">Kormat Alir Tasty</h3>
+        <h3 className="companies_name">Halal Vendor</h3>
+        <h3 className="companies_name">Alal Halal</h3>
+        <h3 className="companies_name">Halal guys</h3>
+        <h3 className="companies_name">Kurban Halal</h3>
+      </div>
+    </div>
+  );
+};
+export default Companies;
