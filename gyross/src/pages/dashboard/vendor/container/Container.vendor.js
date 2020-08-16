3cd64bd0dbd3f2c167a75842f3d52de593b4dd8b
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-08-15 22:29:41 -0400

    Vendor profile container added and style updated

---
+++ gyross/src/pages/dashboard/vendor/container/Container.vendor.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import LeftNav from "../left-nav/LeftNav.vendor";
+import HeaderVendor from "../header/Header.vendor";
+import Profile from "../profile/Profile.vendor";
+
+import FooterVendor from "../footer/Footer.vendor";
+
+import "./Container.vendor.style.scss";
+
+class ContainerVendor extends Component {
+  render() {
+    return (
+      <div className="container-vendor">
+        <div className="container-header">
+          <HeaderVendor />
+        </div>
+        <div className="container-body">
+          <div className="left-nav">
+            <LeftNav />
+          </div>
+          <div className="right-content">
+            <Profile />
+          </div>
+        </div>
+        <div className="container-footer">
+          <FooterVendor />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContainerVendor;
