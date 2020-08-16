9a6594adda240131979f7e8bf2fb6cedbc3a72ca
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-08-15 22:28:51 -0400

    Vendor footer added and style updated

---
+++ gyross/src/pages/dashboard/vendor/footer/Footer.vendor.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.vendor.style.scss";
+import { Link } from "react-router-dom";
+const FooterVendor = () => {
+  return (
+    <div className="vendor-footer-container">
+      <h1 className="footer-title">Vendor</h1>
+      <nav>
+        <div className="left">
+          <h1 className="label">Help</h1>
+          <Link>Item 1</Link>
+          <Link>Item 2</Link>
+          <Link>Item 1</Link>
+          <Link>Item 2</Link>
+        </div>
+        <div className="right">
+          <h1 className="label">Quick Link</h1>
+          <Link>Item 1</Link>
+          <Link>Item 2</Link>
+          <Link>Item 1</Link>
+          <Link>Item 2</Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default FooterVendor;
