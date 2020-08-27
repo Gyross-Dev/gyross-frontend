72a029e985ff166ff82c4aa3a4c76aabfb73dbe2
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-08-26 21:48:16 -0400

    Vendore profile reducer added to root reducer

--- gyross/src/redux/reducers/index.js
+++ gyross/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import VendorAuth from "./vendor/Auth.vendor.reducer";
 import NameLocation from "./vendor/Name-Location.reducer";
+import vendorProfile from "./vendor/Profile.vendor.reducer";
 
 const rootReducer = combineReducers({
   Vendor: VendorAuth,
   nameLocation: NameLocation,
+  vendorProfile: vendorProfile,
 });
 
 export default rootReducer;
