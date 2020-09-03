15ddc916eae7071cdc8bafade871bbbaa3256ea5
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-09-02 20:19:25 -0400

    Re constructed menu's data structure

--- gyross/src/components/menus/Menus.component.js
+++ gyross/src/components/menus/Menus.component.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import Mapped from "./mapped/Mapped";
+import { MapMenu, MapSauce } from "./mapped/Mapped";
 import { connect } from "react-redux";
 import { getMenus } from "../../redux/actions/vendor/Profile.vendor.action";
 
 class Menus extends Component {
   state = {
-    menu: null,
+    breakfast: null,
+    dinner: null,
+    lunch: null,
+    soda: null,
+    sauce: null,
   };
   componentDidMount() {
     this.setState({ menu: this.props.menu });
@@ -14,25 +18,42 @@ class Menus extends Component {
   }
   componentDidUpdate(prevProps) {
     if (prevProps.menu !== this.props.menu) {
-      this.setState({ menu: this.props.menu });
+      let { breakfast, dinner, lunch, soda, sauce } = this.props.menu;
+      this.setState({
+        breakfast,
+        lunch,
+        dinner,
+        soda,
+        sauce,
+      });
     }
   }
 
   render() {
+    let { breakfast, dinner, lunch, soda, sauce } = this.state;
     return (
       <div className="menu-container">
         <div className="menu-wrapper">
           <h1 className="menu-title">Menu</h1>
-          {this.state.menu
-            ? Object.entries(this.state.menu).map(([key, val], index) => {
-                return (
-                  <div className="menu-box" key={index}>
-                    <div className="menu-category">{key}</div>
-                    <Mapped item={key} val={val} />
-                  </div>
-                );
-              })
-            : null}
+          <div className="category">
+            {breakfast ? <MapMenu menu="breakfast" val={breakfast} /> : null}
+          </div>
+
+          <div className="category">
+            {lunch ? <MapMenu menu="lunch" val={lunch} /> : null}
+          </div>
+
+          <div className="category">
+            {dinner ? <MapMenu menu="dinner" val={dinner} /> : null}
+          </div>
+
+          <div className="category">
+            {soda ? <MapMenu menu="soda" val={soda} /> : null}
+          </div>
+
+          <div className="category">
+            {sauce ? <MapSauce menu="sauce" val={sauce} /> : null}
+          </div>
         </div>
       </div>
     );
--- gyross/src/components/menus/mapped/Mapped.js
+++ gyross/src/components/menus/mapped/Mapped.js
@@ -1,37 +1,37 @@
 import React from "react";
 import "../Menu.style.scss";
 
-const Mapped = ({ val, item }) => {
+export const MapMenu = ({ val, menu }) => {
   return (
-    <div className="item-container">
-      {val
-        ? Object.entries(val).map(([key, val], index) => {
-            return (
-              <div className="item" key={index}>
-                <span>{key}</span>
-                <MapChild val={val} item={key} />
-              </div>
-            );
-          })
-        : null}
-    </div>
-  );
-};
-// line 7 && item !== "sauce"
-const MapChild = ({ val }) => {
-  return (
-    <div>
-      {val
-        ? Object.entries(val).map(([key, val], index) => {
-            return (
-              <div className="price" key={(index, key)}>
-                <span>$ {val}</span>
-              </div>
-            );
-          })
-        : null}
+    <div className="category-container">
+      <div className="category-title">{menu}</div>
+      <div className="category-card">
+        {val.map((item, index) => {
+          return (
+            <div className="category-items" key={index}>
+              <div className="item">{item["name"]}</div>
+              <div className="price">$ {item["price"]}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Mapped;
+export const MapSauce = ({ menu, val }) => {
+  return (
+    <div className="category-container">
+      <div className="category-title">{menu}</div>
+      <div className="category-card">
+        {val.map((sauce, i) => {
+          return (
+            <div className="category-items" key={i}>
+              <div className="item">{sauce}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
